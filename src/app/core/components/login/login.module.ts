49576df6bcef9b91e4dcf2c87e4ae39340868894
Grzegorz Kniażuk <grzegorz.kniazuk@gmail.com>
2018-08-21 14:17:19 +0200

    update 21.08

--- src/app/core/components/login/login.module.ts
+++ src/app/core/components/login/login.module.ts
@@ -4,14 +4,12 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import { LoginRecoveryComponent} from './login-recovery/login-recovery.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../../shared/shared.module';
-import { LoginRouteModule } from './login-route.module';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     ReactiveFormsModule,
     SharedModule,
-    LoginRouteModule,
     RouterModule,
   ],
   declarations: [
